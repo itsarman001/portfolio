080c05b07f793394757ea558fb6a3aac54ed7bdc
Arman <armanar6712@gmail.com>
2024-07-19 08:25:09 +0530

    initial state

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold underline underline-offset-1 text-center">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default App;
